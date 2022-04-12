9ec3f1d2898cde2ad07cb0e274c40128c8e45fa9
Taksh Patel <88882602+TakshPatel26@users.noreply.github.com>
2022-04-12 16:46:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status="";
+input_text="";
+function setup(){
+    canvas=createCanvas(300,290);
+    canvas.position(480,250);
+    video=createCapture(VIDEO);
+    video.size(300,290);
+    video.hide();
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    input_text=document.getElementById("object_name").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+function draw(){
+    image(video,0,0,300,290)
+}
